87e0ab22f6de5eabb49a507698cd1087da695b8d
thkocks <timhkocks@gmail.com>
2017-06-28 12:44:08 +0200

    Fix seeds syntax errors

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.destoy_all
+User.destroy_all
 Student.destroy_all
 
 teacher1 = User.create(email: "teacher1@codaisseur", password:"qwer1234")
 teacher2 = User.create(email: "teacher2@codaisseur", password:"qwer1234")
 teacher3 = User.create(email: "teacher3@codaisseur", password:"qwer1234")
 
-student = Student.create(name: "Dave Fletcher", Image_url: "https://goo.gl/rN13io", batch: 1)
-student = Student.create(name: "Miriam Donovan", Image_url: "https://goo.gl/rN13io", batch: 1)
-student = Student.create(name: "Anna Erkisson", Image_url: "https://goo.gl/rN13io", batch: 2)
-student = Student.create(name: "Steve Bushemi", Image_url: "https://goo.gl/rN13io", batch: 2)
-student = Student.create(name: "Markus Judeski", Image_url: "https://goo.gl/rN13io", batch: 3)
-student = Student.create(name: "Wendy Mussel", Image_url: "https://goo.gl/rN13io", batch: 3)
+student = Student.create(name: "Dave Fletcher", image_url: "https://goo.gl/rN13io", batch: 1)
+student = Student.create(name: "Miriam Donovan", image_url: "https://goo.gl/rN13io", batch: 1)
+student = Student.create(name: "Anna Erkisson", image_url: "https://goo.gl/rN13io", batch: 2)
+student = Student.create(name: "Steve Bushemi", image_url: "https://goo.gl/rN13io", batch: 2)
+student = Student.create(name: "Markus Judeski", image_url: "https://goo.gl/rN13io", batch: 3)
+student = Student.create(name: "Wendy Mussel", image_url: "https://goo.gl/rN13io", batch: 3)
